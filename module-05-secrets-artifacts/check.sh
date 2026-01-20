abd1043f17c8867bbc3dc15840f3445c1266fa74
Irfan Ullah <raiirfan@gmail.com>
2026-01-20 15:27:28 +0400

    fix: generate artifact in correct path

--- module-05-secrets-artifacts/check.sh
+++ module-05-secrets-artifacts/check.sh
@@ -3,6 +3,6 @@
 echo "Running Module 5 checks"
 echo "Environment is: $APP_ENV"
 
-echo "CI artifact generated" > artifact.txt
+echo "CI artifact generated" > module-05-secrets-artifacts/artifact.txt
 
 exit 0
